19c453672632ab869292f2a2bf7cbb312ab69e1a
Ethan <aurora314156@gmail.com>
2019-02-24 07:28:19 -0800

    none

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <iostream>
+#include <ctime>
+#define size 100
+using namespace std;
+
+
+
+int main() {
+  string a = "abc";
+  cout<<a[0]<<endl;
+  
+  cout<<a.length()<<endl;
+
+  
+  return 0;
+}
