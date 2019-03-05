34b3eae03dcfb049d21212e872631a72f23b8e8b
Ethan <aurora314156@gmail.com>
2019-03-05 06:02:59 -0800

    Read the fifth chapter

--- main.cpp
+++ main.cpp
@@ -1,17 +1,29 @@
-#include <string.h>
-#include <iostream>
-#include <ctime>
-#define size 100
-using namespace std;
 
+// UVa489 Hangman Judge
+// Rujia Liu
+#include<stdio.h>
+#include<string.h>
+#define maxn 100
+int left, chance;
+char s[maxn], s2[maxn];
+int win, lose;
 
 
 int main() {
-  string a = "abc";
-  cout<<a[0]<<endl;
-  
-  cout<<a.length()<<endl;
+  int rnd;
+  while(scanf("%d%s%s", &rnd, s, s2) == 3 && rnd != -1) {
+    printf("%d\n",strlen(s));
+    for(int i =0;i<strlen(s);i++)
+    {
+      for(int j =i;j<strlen(s);j++)
+      {
+        
+      }
+    }
 
-  
+    if(win) printf("You win.\n");
+    else if(lose) printf("You lose.\n");
+    else printf("You chickened out.\n");
+  }
   return 0;
 }
